6e5e52036a2226071e574afe71c588ac4bdf780a
Adeebshad <adeebbinislam@gmail.com>
2023-06-05 22:40:46 +0600

    DbContext Add

---
+++ JwtAuthentication/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using JwtAuthentication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtAuthentication.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Team> Teams { get; set; }
+    }
+}
+ 
