639c85364bebf94136e917cfbff6503b944a89b5
Adeebshad <csadeebshad@gmail.com>
2023-06-05 16:17:46 +0600

    first commit

---
+++ JwtAuthentication/Controllers/TeamsController.cs
@@ -0,0 +1,78 @@
+﻿using JwtAuthentication.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TeamsController : ControllerBase
+    {
+        private static List<Team> teams = new List<Team>()
+        {
+            new Team()
+            {
+                Country = "Faa",
+                Id = 1,
+                Name= "Test"
+            },
+            new Team()
+            {
+                 Country = "TaD",
+                Id = 2,
+                Name= "Nhbs"
+            },
+            new Team()
+            {
+                 Country = "Philipain",
+                Id = 3,
+                Name= "Jkkm"
+            }
+        };
+
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            return Ok(teams);
+        }
+
+        [HttpGet("{id:int}")]
+
+        public IActionResult Get(int id)
+        {
+            var team = teams.FirstOrDefault(x => x.Id == id);
+            if (team == null) {
+                return BadRequest("Invalid requset");
+            }
+            return Ok(team);
+
+        }
+        [HttpPost]
+        public IActionResult Post(Team team)
+        {
+            teams.Add(team);
+            return CreatedAtAction("Get", team.Id, team);
+        }
+
+        [HttpPatch]
+        public IActionResult Patch(int id, string name) {
+            var team = teams.FirstOrDefault(x => x.Id == id);
+            if (team == null)
+                return BadRequest("Invalid");
+
+
+            team.Name = name;
+            return NoContent();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int id) {
+            var team = teams.FirstOrDefault(x => x.Id == id);
+            if (team == null)
+                return BadRequest("Invalid");
+
+            teams.Remove(team);
+            return NoContent();
+        }
+    }
+}
---
+++ JwtAuthentication/Models/Team.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtAuthentication.Models
+{
+    public class Team
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Country { get; set; }
+    }
+}
